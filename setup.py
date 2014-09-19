42edae27af198b30fd3f0c2fb40f00b9f93edf4e
Laxentis <laxentis@gmail.com>
2014-09-19 18:16:23 +0200

    Win build was wrong - new build with py2exe

--- setup.py
+++ setup.py
@@ -1,16 +1,4 @@
-import sys
-from cx_Freeze import setup, Executable
+from distutils.core import setup
+import py2exe
 
-build_exe_options = {"packages": ["os"], "excludes": []}
-
-base = None
-
-if sys.platform == "win32":
-    base =  "Win32GUI"
-
-setup (
-        name = "pySARPP",
-        version = "0.1",
-        description = "DCS MiG21bis SARPP interpreter",
-        options = {"build_exe": build_exe_options},
-        executables = [Executable("Sarpp.py", base=base)])
+setup(console=['Sarpp.py'])
